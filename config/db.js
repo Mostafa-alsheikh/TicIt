398b4d84c1985b13b6ee4a1a73c0e221222aafb7
Mostafa Mohamed <M.Mohamed2262@nu.edu.eg>
2026-03-27 08:42:13 +0200

    update Github Actions

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const { mongoUri} = require('./index')
+const { mongoUri } = require('./index');
 
 const connectDB = async () => {
     if (mongoose.connection.readyState !== 0) return;
     try {
         await mongoose.connect(mongoUri);
         console.log('MongoDB connected');
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error.message);
-        process.exit(1);
+        if (process.env.NODE_ENV !== 'test') {
+            process.exit(1);
+        }
     }
 };
-    module.exports = connectDB;
+
+module.exports = connectDB;
